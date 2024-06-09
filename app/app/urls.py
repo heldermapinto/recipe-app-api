354dd5417b8b9738f6fee650013590be92cb418a
Helder Manuel Afonso Pinto <helder.manuel.afonso.pinto@sap.com>
2024-06-09 17:40:36 +0200

    create recipe

--- app/app/urls.py
+++ app/app/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
-    path('api/user/', include('user.urls'))
+    path('api/user/', include('user.urls')),
+    path('api/recipe/', include('recipe.urls'))
 ]
